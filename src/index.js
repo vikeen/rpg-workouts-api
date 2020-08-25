2e479c1be6423cf16caed5cbbafa16486d2a85cc
John Rake <john.rake12@gmail.com>
2020-08-25 18:17:22 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import restify from 'restify'
+import 'dotenv/config'
+
+const server = restify.createServer({
+    name: 'rpg-workouts',
+    version: '1.0.0'
+});
+
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+
+server.get('/echo/:name', function (req, res, next) {
+    res.send(req.params);
+    return next();
+});
+
+server.listen(8080, function () {
+    console.log('%s listening at %s', server.name, server.url);
+});
